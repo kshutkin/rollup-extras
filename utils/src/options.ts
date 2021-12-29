da5c9f086580743302baaab3e55cb4b38a1e9a9c
Konstantin Shutkin <kostya80@gmail.com>
2021-12-29 23:01:33 +0400

    fix: allow arrays of records as options

--- utils/src/options.ts
+++ utils/src/options.ts
@@ -1,12 +1,12 @@
 import { DefaultsFactory, Result, SimpleOptions } from './types';
 
-export function getOptions<T extends string | string[] | undefined | Record<string, unknown>, D, F extends DefaultsFactory<Partial<{[K in C]: string[]}> & Partial<Exclude<T, SimpleOptions>>>, C extends string>(options: T | undefined, defaults: D | undefined, field: C, factory?: F) {
+export function getOptions<T extends string | string[] | undefined | Record<string, unknown> | Record<string, unknown>[], D, F extends DefaultsFactory<Partial<{[K in C]: string[]}> & Partial<Exclude<T, SimpleOptions>>>, C extends string>(options: T | undefined, defaults: D | undefined, field: C, factory?: F) {
     const newOptions = recursiveArrayOptions(options, field);
     
     return getOptionsObject((newOptions ? (newOptions as unknown as {[K in C]: string[]}) : (options as Exclude<T, SimpleOptions>)) as Partial<{[K in C]: string[]}> & Partial<Exclude<T, SimpleOptions>>, defaults, factory);
 }
 
-function recursiveArrayOptions<T extends string | string[] | undefined | Record<string, unknown>, C extends string>(options: T, field: C): undefined | { [K in C]: string[] } {
+function recursiveArrayOptions<T extends string | string[] | undefined | Record<string, unknown> | Record<string, unknown>[], C extends string>(options: T, field: C): undefined | { [K in C]: string[] } {
     if (typeof options === 'string') {
         return { [field]: [options] } as unknown as { [K in C]: string[] };
     } else if (Array.isArray(options)) {
