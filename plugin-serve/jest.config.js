4b859f742269edf685548006ab6733884ad29910
Konstantin Shutkin <kostya80@gmail.com>
2021-12-29 22:00:18 +0400

    feat: use pkgbld, newer project template and update dependencies (#5)
    * fix: use pkgbld and newer logger
    
    * fix: use newer appender version
    
    * update lock file
    
    * fix: remove unnececary config file + linting fixes
    
    * fix: use pkgbld + updated fields in package.json
    
    * feat: use pkgbld

--- plugin-serve/jest.config.js
+++ plugin-serve/jest.config.js
@@ -1,19 +1,19 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  testMatch: ['<rootDir>/**/tests/**/*.spec.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    testMatch: ['<rootDir>/**/tests/**/*.spec.ts'],
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
     },
-  },
-  testPathIgnorePatterns: ['/node_modules/'],
-  coverageDirectory: './coverage',
-  coveragePathIgnorePatterns: ['node_modules', 'src/tests'],
-  moduleNameMapper: {
-    "@rollup-extras/(.*)/(.*)": "<rootDir>/../$1/src/$2.ts",
-    "@rollup-extras/(.*)$": "<rootDir>/../$1/src",
-  },
+    testPathIgnorePatterns: ['/node_modules/'],
+    coverageDirectory: './coverage',
+    coveragePathIgnorePatterns: ['node_modules', 'src/tests'],
+    moduleNameMapper: {
+        '@rollup-extras/(.*)/(.*)': '<rootDir>/../$1/src/$2.ts',
+        '@rollup-extras/(.*)$': '<rootDir>/../$1/src',
+    },
 };
