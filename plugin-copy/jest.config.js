3031d3619c31dbde834b115897aeb8e61cdce352
Konstantin Shutkin <kostya80@gmail.com>
2023-04-29 20:43:26 +0200

    chore: update jest configs

--- plugin-copy/jest.config.js
+++ plugin-copy/jest.config.js
@@ -1,14 +1,8 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     extensionsToTreatAsEsm: ['.ts'],
     testMatch: ['<rootDir>/**/tests/**/*.spec.ts'],
-    globals: {
-        'ts-jest': {
-            useESM: true,
-        },
-    },
     testPathIgnorePatterns: ['/node_modules/'],
     coverageDirectory: './coverage',
     coveragePathIgnorePatterns: ['node_modules', 'src/tests'],
@@ -16,4 +10,9 @@ export default {
         '@rollup-extras/(.*)/(.*)': '<rootDir>/../$1/src/$2.ts',
         '@rollup-extras/(.*)$': '<rootDir>/../$1/src',
     },
+    transform: {
+        '\\.ts$': ['ts-jest', {
+            useESM: true
+        }]
+    }
 };
--- plugin-externals/jest.config.js
+++ plugin-externals/jest.config.js
@@ -1,28 +1,18 @@
-import fs from 'fs';
-
-const pkg = JSON.parse(fs.readFileSync('package.json').toString());
-const scopeTest = /(@.+)\/.+$/g.exec(pkg.name);
-let scope = undefined;
-let moduleNameMapper = {};
-if (scopeTest && scopeTest[0]) {
-    scope = scopeTest[1];
-    moduleNameMapper[`${scope}/(.*)/(.*)`] = '<rootDir>/../$1/src/$2.ts';
-    moduleNameMapper[`${scope}/(.*)$`] = '<rootDir>/../$1/src';
-}
-
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     extensionsToTreatAsEsm: ['.ts'],
     testMatch: ['<rootDir>/**/tests/**/*.spec.ts'],
-    globals: {
-        'ts-jest': {
-            useESM: true,
-        },
-    },
     testPathIgnorePatterns: ['/node_modules/'],
     coverageDirectory: './coverage',
     coveragePathIgnorePatterns: ['node_modules', 'src/tests'],
-    moduleNameMapper
+    moduleNameMapper: {
+        '@rollup-extras/(.*)/(.*)': '<rootDir>/../$1/src/$2.ts',
+        '@rollup-extras/(.*)$': '<rootDir>/../$1/src',
+    },
+    transform: {
+        '\\.ts$': ['ts-jest', {
+            useESM: true
+        }]
+    }
 };
